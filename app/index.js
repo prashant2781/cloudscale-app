3f0b3a46741bf6dcc084f287cc4f148fd09508fb
Prashant Saxena <Prashant.Saxena@peabody.org.uk>
2026-03-30 10:07:22 +0530

    Test CI/CD auto deployment

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.status(200).send("✅ CloudScale App is running");
+  res.status(200).send("CloudScale App deployed via CI/CD ✅");
 });
 
 app.get("/health", (req, res) => {
